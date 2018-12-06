0d1858ca3e62f3cdae9bb4814bd73d253dfcb8f6
Asutosh <Asutoshp11@yahoo.com>
2018-12-06 18:09:25 +0530

    done

---
+++ fileio.py
@@ -0,0 +1,35 @@
+
+a = 33
+b = 200
+k=1
+arr=[11,22,33]
+
+  
+
+def func1(i,j):
+    return i+j
+
+
+if b > a:
+    print("b is greater than a")
+
+for x in range(5,21,2):
+    print(x)
+k=0
+for x in arr:
+    print(x)
+    print(arr[k])
+    print(k)
+    k=k+1
+    
+val=func1(6,8)
+print(val)
+k=1
+
+f=open("/home/asu/Python/logs/log.txt")
+#print (f.read()) - write all
+for x in f:
+ 	print(x)	
+
+
+    
